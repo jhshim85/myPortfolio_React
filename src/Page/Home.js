5bd407027ae0147109bb5e6d4586a98373783742
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2023-07-29 01:30:45 -0600

    basic home page done

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,15 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
 const Home = () => {
   return (
-    <div class="wrapper heading__container">
-      <h1 class="heading__greeting">Hello!</h1>
-      <h2 class="heading__name">I'm Jaehyun(Jay).
-      <br /><span class="heading__occupation">Front-End Developer</span></h2>
-      <h3 class="heading__intro">I am a front-end web developer focused on planning, designing and creating simple & user-friendly products that impact and improve the lives of those around me.</h3>
-      <a class="heading__button" href="#about">
-        <span class="heading__button--text">more about me</span>
-        <span class="heading__button--icon fa fa-arrow-right"></span>
-      </a>
-    </div>
+    <main className="page__theme--dark">
+      <div className="wrapper heading__container">
+        <h1 className="heading__greeting">Hello!</h1>
+        <h2 className="heading__name">I'm Jaehyun(Jay).
+        <br /><span className="heading__occupation">Front-End Developer</span></h2>
+        <h3 className="heading__intro">I am a front-end web developer focused on planning, designing and creating simple & user-friendly products that impact and improve the lives of those around me.</h3>
+        <a className="heading__button" href="#about">
+          <p className="heading__button--text">more about me</p>
+          <FontAwesomeIcon className="heading__button--icon" icon={faArrowLeft} />
+        </a>
+      </div>
+    </main>
   )
 }
 
